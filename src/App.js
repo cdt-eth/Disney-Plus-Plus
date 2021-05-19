101672c983fb96d4342f56174edc709bd3a39c10
christianturner <christiandavidphoto@gmail.com>
2021-05-19 11:51:04 -0400

    home slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import HomeSlider from "./components/Slider";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <HomeSlider />
     </div>
   );
 }
