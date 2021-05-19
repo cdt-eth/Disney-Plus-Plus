c41a3c1c8e1d5bb9ce4dbf007a87900c7a89213f
christianturner <christiandavidphoto@gmail.com>
2021-05-19 14:29:27 -0400

    studio cards added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import HomeSlider from "./components/Slider";
+import HomeSlider from "./components/HomeSlider";
+import Studios from "./components/Studios";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <HomeSlider />
+      <Studios />
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,12 +8,10 @@ import {
   MdTv as TvIcon,
   MdLocalMovies as MovieIcon,
 } from "react-icons/md";
-// import Logo from "./logo.svg";
 
 export default function Nav() {
   return (
     <nav className="nav">
-      {/* <img src={Logo} /> */}
       <a href="www.com" className="logo">
         <img
           src="https://prod-static.disney-plus.net/us-west-2/builds/e76798ab2a732a884562763cbd19b969a80dcd5b_1606148151180/images/logo.svg"
