484fe06fff4cd4c157c29493cb8bf46140e87b5b
christianturner <christiandavidphoto@gmail.com>
2021-05-20 21:25:02 -0400

    watchlist ui

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,7 +1,15 @@
+import { MdAddCircleOutline as AddIcon } from "react-icons/md";
+
 export default function Watchlist() {
   return (
     <div className="page">
-      <h1 className="pageTitle">Watchlist</h1>
+      {/* <h1 className="pageTitle">Watchlist</h1> */}
+
+      <div className="watchlistWrapper">
+        <AddIcon />
+        <h3>Your watchlist is empty</h3>
+        <p>Content you add to your watchlist will appear here.</p>
+      </div>
     </div>
   );
 }
