b6d2c300ba265ecccf67503f8f5246e79ab49cc8
christianturner <christiandavidphoto@gmail.com>
2021-05-20 21:04:31 -0400

    search page ui

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,90 @@
 export default function Search() {
   return (
-    <div className="page">
-      <h1 className="pageTitle">Explore</h1>
+    <div>
+      <form action="submit" className="form">
+        <input
+          placeholder="Search by title, character, or genre"
+          className="input"
+        />
+      </form>
+      <div className="page">
+        <h1 className="pageTitle">Explore</h1>
+
+        <div className="results">
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian "
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian "
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian "
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian "
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian"
+            />
+          </div>
+          <div className="result">
+            <img
+              src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/267252F276A5CB826A8AB77FB70B5C4EE8EA0E529DE9C82C79D0F85C899D5FB9/badging?width=800&aspectRatio=1.78&format=jpeg&label=originals"
+              alt="Mandalorian "
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
