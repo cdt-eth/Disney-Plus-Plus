23fb938adf3d3b6049af65d8416f66bd0db08328
christianturner <christiandavidphoto@gmail.com>
2021-06-05 23:08:15 -0400

    update video params capitalization

--- src/components/Studios/Studios.jsx
+++ src/components/Studios/Studios.jsx
@@ -8,7 +8,7 @@ export default function Studios() {
           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/FFA0BEBAC1406D88929497501C84019EBBA1B018D3F7C4C3C829F1810A24AD6E/scale?width=640&aspectRatio=1.78&format=png"
           alt="Disney"
         />
-        <video loop="true" autoplay="true" playsinline="true">
+        <video loop={true} autoPlay={true} playsInline={true}>
           <source
             src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217865-disney.mp4"
             type="video/mp4"
@@ -21,7 +21,7 @@ export default function Studios() {
           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F4E1A299763030A0A8527227AD2812C049CE3E02822F7EDEFCFA1CFB703DDA5/scale?width=640&aspectRatio=1.78&format=png"
           alt="Pixar"
         />
-        <video loop="true" autoplay="true" playsinline="true">
+        <video loop={true} autoPlay={true} playsInline={true}>
           <source
             src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217992-pixar.mp4"
             type="video/mp4"
@@ -34,7 +34,7 @@ export default function Studios() {
           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C90088DCAB7EA558159C0A79E4839D46B5302B5521BAB1F76D2E807D9E2C6D9A/scale?width=640&aspectRatio=1.78&format=png"
           alt="Marvel"
         />
-        <video loop="true" autoplay="true" playsinline="true">
+        <video loop={true} autoPlay={true} playsInline={true}>
           <source
             src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217799-marvel.mp4"
             type="video/mp4"
@@ -47,7 +47,7 @@ export default function Studios() {
           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=640&aspectRatio=1.78&format=png"
           alt="Star Wars"
         />
-        <video loop="true" autoplay="true" playsinline="true">
+        <video loop={true} autoPlay={true} playsInline={true}>
           <source
             src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2020/12/17/1608229334-star-wars.mp4"
             type="video/mp4"
@@ -60,7 +60,7 @@ export default function Studios() {
           src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/2EF24AA0A1E648E6D1A3B26491F516632137ED87AB22969D153316F8BD670FB5/scale?width=640&aspectRatio=1.78&format=png"
           alt="NatGeo"
         />
-        <video loop="true" autoplay="true" playsinline="true">
+        <video loop={true} autoPlay={true} playsInline={true}>
           <source
             src="https://vod-bgc-na-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/07/1565217923-national-geographic.mp4"
             type="video/mp4"
