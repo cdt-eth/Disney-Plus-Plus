b510309967ac3ee31aefb132870131bd1bb4ee1b
christianturner <christiandavidphoto@gmail.com>
2021-06-06 21:48:54 -0400

    genre ids array converted to genre names

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,15 +1,41 @@
 import "./ResultPage.css";
 // import { useParams } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+// import { useState } from "react";
 import { FaPlay as PlayIcon } from "react-icons/fa";
 
 export default function ResultPage(props) {
+  const [genreNames, setGenreNames] = useState([]);
   //   const { id } = useParams();
-  const { poster, overview, title, alt, release_date, genre_ids } =
+  const { poster, overview, title, alt, release_date, genres } =
     props.location.state;
   const date = release_date.substr(0, release_date.indexOf("-"));
 
-  console.log(genre_ids);
+  //   console.log("props genres:", genres);
+
+  useEffect(() => {
+    // console.log("useEffect Ran!");
+
+    const fetchGenres = async () => {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=1dbf27409e387afe9abadb77b2745ddd"
+      );
+      const data = await res.json();
+      const apiGenres = data.genres;
+
+      const filtered = [];
+      apiGenres.map((res) => {
+        if (genres.includes(res.id)) {
+          filtered.push(res.name);
+        }
+        return filtered;
+      });
+
+      setGenreNames(filtered);
+    };
+
+    fetchGenres();
+  }, [genres]);
 
   return (
     <div className="resultPage">
@@ -39,12 +65,7 @@ export default function ResultPage(props) {
           </div>
 
           <h3>{date} </h3>
-          {/* <h4>
-            Genres:{" "}
-            {genre_ids.map((genre) => {
-              return <li>{genre}</li>;
-            })}
-          </h4> */}
+          <div>Genres: {genreNames.join(", ")}</div>
           <h5>{overview}</h5>
         </div>
       </div>
