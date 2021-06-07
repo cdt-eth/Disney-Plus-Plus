d559c8609fcb3ff282612fca3efc782a04830842
christianturner <christiandavidphoto@gmail.com>
2021-06-07 00:01:22 -0400

    add result page buttons and fix for mobile

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,21 +1,16 @@
 import "./ResultPage.css";
-// import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import { useState } from "react";
-import { FaPlay as PlayIcon } from "react-icons/fa";
+import { FaPlay as PlayIcon, FaPlus as PlusIcon } from "react-icons/fa";
+import { IoIosPeople as PeopleIcon } from "react-icons/io";
 
 export default function ResultPage(props) {
   const [genreNames, setGenreNames] = useState([]);
-  //   const { id } = useParams();
+
   const { poster, overview, title, alt, release_date, genres } =
     props.location.state;
   const date = release_date.substr(0, release_date.indexOf("-"));
 
-  //   console.log("props genres:", genres);
-
   useEffect(() => {
-    // console.log("useEffect Ran!");
-
     const fetchGenres = async () => {
       const res = await fetch(
         "https://api.themoviedb.org/3/genre/movie/list?api_key=1dbf27409e387afe9abadb77b2745ddd"
@@ -62,10 +57,19 @@ export default function ResultPage(props) {
               <PlayIcon />
               <p>Trailer</p>
             </a>
+
+            <a href="www.com" className="circleButton">
+              <PlusIcon />
+            </a>
+
+            <a href="www.com" className="circleButton">
+              <PeopleIcon />
+            </a>
           </div>
 
-          <h3>{date} </h3>
-          <div>Genres: {genreNames.join(", ")}</div>
+          <div>
+            {date} • {genreNames.join(", ")}
+          </div>
           <h5>{overview}</h5>
         </div>
       </div>
