592db9a96086a465699fecd4a18c50b5255bd317
christianturner <christiandavidphoto@gmail.com>
2021-06-07 09:48:05 -0400

    add Popular items to homepage carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,24 +1,63 @@
 import "./Carousel.css";
+import { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Carousel() {
+  const [popular, setPopular] = useState([]);
   const settings = {
     dots: true,
     infinite: true,
     autoplay: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
     centerMode: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 812,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
   };
+
+  useEffect(() => {
+    const fetchPopular = async () => {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=1dbf27409e387afe9abadb77b2745ddd&language=en-US&page=1"
+      );
+      const data = await res.json();
+      const results = data.results;
+      console.log(results);
+
+      setPopular(results);
+    };
+
+    fetchPopular();
+  }, []);
 
   return (
     <div className="slideWrapper">
       <Slider {...settings}>
-        <div className="banner">
+        {popular.map((p) => {
+          return (
+            <div className="banner" key={p.title}>
+              <img
+                src={
+                  p.poster_path
+                    ? `https://image.tmdb.org/t/p/original/${p.poster_path}`
+                    : "https://www.genius100visions.com/wp-content/uploads/2017/09/placeholder-vertical.jpg"
+                }
+                alt={p.title}
+              />
+            </div>
+          );
+        })}
+        {/* <div className="banner">
           <img
             src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F2714C42107E421DB04F96D1C4E269955A7F578C59B614505FE499FDC8A08CA/scale?width=2880&aspectRatio=3.91&format=jpeg"
             alt="bad batch"
@@ -53,7 +92,7 @@ export default function Carousel() {
             src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F2714C42107E421DB04F96D1C4E269955A7F578C59B614505FE499FDC8A08CA/scale?width=2880&aspectRatio=3.91&format=jpeg"
             alt="bad batch"
           />
-        </div>
+        </div> */}
       </Slider>
     </div>
   );
