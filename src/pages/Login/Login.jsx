b36f8fe01f7e853be8adb76006aa3bed874b7f82
christianturner <christiandavidphoto@gmail.com>
2021-06-11 11:02:14 -0400

    placeholder content for future Login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,21 @@
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <div className="loginPage">
+      <h3>Login content</h3>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis maximus
+        magna, ac fermentum nisl. Suspendisse potenti. Praesent eget arcu quis
+        urna vehicula tempus. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Morbi eleifend pellentesque
+        magna, at gravida nisl. Ut suscipit sed nisl a facilisis. Nunc a maximus
+        erat, in ultricies justo. Sed blandit faucibus nisi sit amet interdum.
+        Curabitur luctus libero felis, vel fringilla purus vulputate in. Proin
+        elementum, magna quis efficitur egestas, tellus turpis feugiat mauris,
+        at ultrices diam sem faucibus lectus. Nulla eget elit ut elit placerat
+        lobortis. Sed vel nibh sodales, posuere nisi in, congue nunc.
+      </div>
+    </div>
+  );
+}
