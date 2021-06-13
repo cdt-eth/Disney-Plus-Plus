37bc4529784ac48b70936814a3d06378b17c3945
christianturner <christiandavidphoto@gmail.com>
2021-06-12 23:34:37 -0400

    add SCSS file for react-modal-video package to lightbox trailer vid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import "react-modal-video/scss/modal-video.scss";
 
 function App() {
   return (
