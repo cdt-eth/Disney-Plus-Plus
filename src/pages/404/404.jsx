f2b05ea1c03c4af68c5b14ecea34306799fb3520
christianturner <christiandavidphoto@gmail.com>
2021-06-20 21:49:40 -0400

    disney++ to mimic site's name

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -7,7 +7,9 @@ export default function NoMatch() {
       <div className="overlay"></div>
       <div className="errorMessage">
         <h1>The page you were looking for cannot be found.</h1>
-        <h2>Please go to the Disney+ home page by clicking the button below</h2>
+        <h2>
+          Please go to the Disney++ home page by clicking the button below
+        </h2>
         <Link to="/">
           <button>Disney++ Home</button>
         </Link>
