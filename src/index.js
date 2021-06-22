d8fee03ce2a95243e287a86c755ca0a4ff3435f9
christianturner <christiandavidphoto@gmail.com>
2021-06-22 11:18:27 -0400

    temporary silencing of react-transition-group legacy context api warning until they fix it, changed StrictMode to Fragments

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+    {/* </React.StrictMode>, */}
+  </>,
   document.getElementById("root")
 );
